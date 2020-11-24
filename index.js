9acff7705415bb1c5fd03e169cf06c5a1092971b
jeremylim91 <jeremylim_91@live.com>
2020-11-24 14:20:09 +0800

    base version of bird watching

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-console.log('hello world');
+// import express
+import express from 'express';
+import pg from 'pg';
+import methodOverride from 'method-override';
+
+// Initialise the DB connection
+const { Pool } = pg;
+// config the connection
+const pgConnectionConfig = {
+  name: 'jeremylim',
+  host: 'localhost',
+  database: 'birding',
+  port: 5432,
+};
+const pool = new Pool(pgConnectionConfig);
+
+// initialise express
+const app = express();
+const PORT = 3004;
+
+// =========middleware configs===================
+app.set('view engine', 'ejs');
+// config to allow use of external CSS stylesheets
+app.use(express.static('public'));
+// config to accept request form data
+app.use(express.urlencoded({ extended: false }));
+// config to allow use of method override with POST having ?_method=PUT
+app.use(methodOverride('_method'));
+
+// ===========specify routes and their reqs/res==========
+
+app.get('/note', (req, res) => {
+  res.render('note');
+});
+
+app.post('/note', (req, res) => {
+  console.log('request for /note came in');
+  console.log('req.body is: ');
+  console.log(req.body);
+  const { fDate, fBehavior, fFlockSize } = req.body;
+  const sqlQuery = 'INSERT INTO notes (date, behavior, flock_size) VALUES ($1, $2, $3)';
+  const inputData = [`${fDate}`, `${fBehavior}`, `${fFlockSize}`];
+  pool.query(sqlQuery, inputData, () => {
+    res.redirect('/');
+  });
+});
+
+app.get('/', (req, res) => {
+  console.log('request for \'/\' came in');
+  // set the sql query
+  const sqlQuery = 'SELECT * FROM notes';
+  // execute the query
+  pool.query(sqlQuery, (queryErr, result) => {
+    if (queryErr) {
+      console.log(`error w/ pool.query: ${err}`);
+      res.status(503).send('Sorry, not found!');
+      return;
+    }
+    console.table(result.rows);
+    res.send(result.rows);
+  });
+});
+
+app.get('/note/:id', (req, res) => {
+  const { id } = req.params;
+  // set the query
+  const sqlQuery = `SELECT * FROM notes WHERE id=${id}`;
+  console.log(`sql query is: ${sqlQuery}`);
+
+  // execute the query
+  pool.query(sqlQuery, (queryErr, result) => {
+    if (queryErr) {
+      console.log(`query error: ${queryErr}`);
+      res.status(503).send('Sorry, not found!');
+      return;
+    }
+    // res.render('note-id', { result });
+    console.table(result.rows[0]);
+    console.log(result.rows[0]);
+    res.render('note-id', result.rows[0]);
+  });
+});
+
+app.listen(PORT);
---
+++ init_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE notes (
+id            SERIAL PRIMARY KEY,   
+date          DATE,                   --Record the date, time and day of the week you are making the observation,
+behavior      TEXT,                   --Take notes on what the bird was doing as you observed it.
+flock_size    INTEGER                -- Try to estimate how many birds are in the flock.
+);
